a4b22360788756986b62e4596c110221874e4c7b
Jannis Pohlmann <jannis.pohlmann@codethink.co.uk>
2013-10-01 18:28:29 +0100

    Add a basic implementation of applying update actions
    Also rework the internals of consonant.store.local.LocalStore a bit
    so that schemas, classes, objects etc. can be loaded from a Git tree
    instead of requiring a commit object. This is important as actions
    in transactions graduatelly create new trees to apply changes but,
    apart from the final commit action, do not create new commits that
    can then be accessed using the public Service API of LocalStore.
    
    The commit also adds new scenario tests for updating an object as
    well as creating and updating an object in separate actions of a
    single transaction. This ensures that target object references
    of both forms, another action or a UUID, are being tested.

--- consonant/store/local.py
+++ consonant/store/local.py
@@ -89,30 +89,101 @@ class LocalStore(services.Service):
     def name(self, commit):
         """Return the name the store has in the given commit."""
 
-        data = self._load_metadata(commit)
+        commit_object = self.repo[commit.sha1]
+        data = self._load_metadata(commit_object.tree)
         return data['name']
 
     def schema(self, commit):
         """Return the schema name the store uses in the given commit."""
 
-        data = self._load_metadata(commit)
-        name = data['schema']
-        url = self.register.schema_url(name)
-        stream = urllib2.urlopen(url)
-        return schema.parsers.SchemaParser().parse(stream)
+        commit_object = self.repo[commit.sha1]
+        return self._schema(commit_object.tree)
 
     def services(self, commit):
         """Return the service aliases used in the store at the given commit."""
 
-        data = self._load_metadata(commit)
+        commit_object = self.repo[commit.sha1]
+        data = self._load_metadata(commit_object.tree)
         return data.get('services', {})
 
     def classes(self, commit):
         """Return the classes present in the given commit of the store."""
 
         commit_object = self.repo[commit.sha1]
+        return self._classes(commit_object.tree)
+
+    def klass(self, commit, name):
+        """Return the class for the given name and commit of the store."""
+
+        commit_object = self.repo[commit.sha1]
+        klass = self._klass(commit_object.tree, name)
+        if not klass:
+            raise services.ClassNotFoundError(commit, name)
+        return klass
+
+    def objects(self, commit, klass=None):
+        """Return the objects present in the given commit of the store."""
+
+        commit_object = self.repo[commit.sha1]
+        return self._objects(commit_object.tree, klass=klass)
+
+    def _schema(self, tree):
+        data = self._load_metadata(tree)
+        name = data['schema']
+        url = self.register.schema_url(name)
+        stream = urllib2.urlopen(url)
+        return schema.parsers.SchemaParser().parse(stream)
+
+    def _klass(self, tree, name):
+        if name != 'consonant.yaml' and name in tree:
+            class_entry = tree[name]
+            object_references = self._class_object_references(class_entry)
+            return objects.ObjectClass(class_entry.name, object_references)
+        else:
+            return None
+
+    def _objects(self, tree, klass=None):
+        schema = self._schema(tree)
+        if klass:
+            return sorted(self._class_objects(tree, schema, klass))
+        else:
+            classes = self._classes(tree)
+            objects = {}
+            for klass in classes.itervalues():
+                class_objects = self._class_objects(tree, schema, klass)
+                objects[klass.name] = sorted(class_objects)
+            return objects
+
+    def object(self, commit, uuid, klass=None):
+        """Return the object with the given UUID from a commit of the store."""
+
+        commit_object = self.repo[commit.sha1]
+        tree = commit_object.tree
+        object = self._object(tree, uuid, klass=klass)
+        if not object:
+            if klass:
+                raise services.ObjectNotFoundError(commit, uuid, klass)
+            else:
+                raise services.ObjectNotFoundError(commit, uuid)
+        return object
+
+    def _object(self, tree, uuid, klass=None):
+        schema = self._schema(tree)
+        if klass:
+            return self._class_object(tree, schema, uuid, klass)
+        else:
+            classes = self._classes(tree)
+            for klass in classes.itervalues():
+                object = self._class_object(tree, schema, uuid, klass)
+                if object:
+                    return object
+            return None
+
+    def _classes(self, tree):
+        """Return the classes present in the given Git tree of the store."""
+
         classes = {}
-        for class_entry in commit_object.tree:
+        for class_entry in tree:
             if class_entry.name == 'consonant.yaml':
                 continue
             object_references = self._class_object_references(class_entry)
@@ -120,59 +191,13 @@ class LocalStore(services.Service):
             classes[class_entry.name] = klass
         return classes
 
-    def klass(self, commit, name):
-        """Return the class for the given name and commit of the store."""
-
-        commit_object = self.repo[commit.sha1]
-        if name != 'consonant.yaml' and name in commit_object.tree:
-            class_entry = commit_object.tree[name]
-            object_references = self._class_object_references(class_entry)
-            return objects.ObjectClass(class_entry.name, object_references)
-        else:
-            raise services.ClassNotFoundError(commit, name)
-
-    def objects(self, commit, klass=None):
-        """Return the objects present in the given commit of the store."""
-
-        schema = self.schema(commit)
-
-        if klass:
-            return sorted(self._class_objects(commit, schema, klass))
-        else:
-            classes = self.classes(commit)
-            objects = {}
-            for klass in classes.itervalues():
-                objects[klass.name] = sorted(
-                    self._class_objects(commit, schema, klass))
-            return objects
-
-    def object(self, commit, uuid, klass=None):
-        """Return the object with the given UUID from a commit of the store."""
-
-        schema = self.schema(commit)
-
-        if klass:
-            object = self._class_object(commit, schema, uuid, klass)
-            if object:
-                return object
-            else:
-                raise services.ObjectNotFoundError(commit, uuid, klass)
-        else:
-            classes = self.classes(commit)
-            for klass in classes.itervalues():
-                object = self._class_object(commit, schema, uuid, klass)
-                if object:
-                    return object
-            raise services.ObjectNotFoundError(commit, uuid)
-
-    def _class_object(self, commit, schema, uuid, klass):
+    def _class_object(self, tree, schema, uuid, klass):
         objects = [x for x in klass.objects if x.uuid == uuid]
         if objects:
-            commit_object = self.repo[commit.sha1]
-            class_entry = commit_object.tree[klass.name]
+            class_entry = tree[klass.name]
             class_tree = self.repo[class_entry.oid]
             object_entry = class_tree[uuid]
-            return self._load_object(commit, schema, klass, object_entry)
+            return self._load_object(tree, schema, klass, object_entry)
         else:
             return None
 
@@ -186,15 +211,14 @@ class LocalStore(services.Service):
             object_references.add(reference)
         return object_references
 
-    def _class_objects(self, commit, schema, klass):
-        """Return the objects of a class in the given commit of the store."""
+    def _class_objects(self, tree, schema, klass):
+        """Return the objects of a class in the given Git tree of the store."""
 
-        commit_object = self.repo[commit.sha1]
-        class_tree_entry = commit_object.tree[klass.name]
+        class_tree_entry = tree[klass.name]
         class_tree = self.repo[class_tree_entry.oid]
         objects = set()
         for object_entry in class_tree:
-            object = self._load_object(commit, schema, klass, object_entry)
+            object = self._load_object(tree, schema, klass, object_entry)
             objects.add(object)
         return objects
 
@@ -220,13 +244,12 @@ class LocalStore(services.Service):
             str(commit.message),
             [x.oid.hex for x in commit.parents])
 
-    def _load_metadata(self, commit):
-        commit_object = self.repo[commit.sha1]
-        entry = commit_object.tree['consonant.yaml']
+    def _load_metadata(self, tree):
+        entry = tree['consonant.yaml']
         blob = self.repo[entry.oid]
         return yaml.load(blob.data)
 
-    def _load_object(self, commit, schema, klass, object_entry):
+    def _load_object(self, tree, schema, klass, object_entry):
         object_tree = self.repo[object_entry.oid]
         properties_entry = object_tree['properties.yaml']
         properties_sha1 = properties_entry.oid.hex
@@ -235,20 +258,17 @@ class LocalStore(services.Service):
             object = self.cache.read_object(object_entry.name, properties_sha1)
         if not object:
             object = self._parse_object(
-                commit, schema, klass, object_entry, properties_entry)
+                tree, schema, klass, object_entry, properties_entry)
         if self.cache:
             self.cache.write_object(object_entry.name, properties_sha1, object)
         return object
 
-    def _parse_object(self, commit, schema, klass, object_entry,
-                      properties_entry):
-        blob = self.repo[properties_entry.oid]
+    def _parse_object(self, tree, schema, klass, object_entry, props_entry):
+        blob = self.repo[props_entry.oid]
         properties_data = yaml.load(blob.data)
-
         props = []
         for name, data in properties_data.iteritems():
             props.append(self._load_property(schema, klass, name, data))
-
         return objects.Object(object_entry.name, klass, props)
 
     def _load_property(self, schema, klass, name, data):
@@ -327,6 +347,8 @@ class TransactionPreparer(object):
     def prepare_transaction(self):
         """Create and return a commit with all changes from the transaction."""
 
+        self.action_objects = {}
+
         # obtain the tree of the source commit the transaction is based on
         source = self.store.commit(self.transaction.begin().source)
         source_object = self.store.repo[source.sha1]
@@ -352,7 +374,8 @@ class TransactionPreparer(object):
 
     def _apply_actions(self, commit, schema, tree):
         for action in self.transaction.actions[1:-1]:
-            tree = self._apply_action(action, commit, schema, tree)
+            obj, tree = self._apply_action(action, commit, schema, tree)
+            self.action_objects[action] = obj
         return tree
 
     def _apply_action(self, action, commit, schema, tree):
@@ -369,19 +392,20 @@ class TransactionPreparer(object):
 
         with Phase() as phase:
             # make sure properties to be set exist in the new object's class
-            self._validate_object_properties(action, schema, phase)
+            self._validate_object_properties(
+                phase, action, schema, action.klass, action.properties)
 
             # make sure none of the properties to set are raw properties
             # as we have separate actions for them
-            self._validate_object_properties_not_raw(action, schema, phase)
-
-        class_entry = tree[action.klass]
-        class_tree = self.store.repo[class_entry.oid]
+            self._validate_object_properties_not_raw(
+                phase, action, schema, action.klass, action.properties)
 
         uuid = self.store.generate_uuid(commit, action.klass)
         object_tree = self._create_object_tree(uuid, action.properties)
 
         # build new class tree with the object added
+        class_entry = tree[action.klass]
+        class_tree = self.store.repo[class_entry.oid]
         builder = self.store.repo.TreeBuilder(class_tree)
         builder.insert(uuid, object_tree.oid, pygit2.GIT_FILEMODE_TREE)
         new_class_oid = builder.write()
@@ -389,26 +413,89 @@ class TransactionPreparer(object):
         # build and return a new overall store tree
         builder = self.store.repo.TreeBuilder(tree)
         builder.insert(action.klass, new_class_oid, pygit2.GIT_FILEMODE_TREE)
-        tree_oid = builder.write()
-        return self.store.repo[tree_oid]
+        new_tree_oid = builder.write()
+        new_tree = self.store.repo[new_tree_oid]
+        klass = self.store._klass(new_tree, action.klass)
+        obj = self.store._object(new_tree, uuid, klass)
+        return obj, new_tree
+
+    def _apply_update_action(self, action, commit, schema, tree):
+        # load the object from the commit or from a previous action
+        obj = self._validate_and_resolve_target_object(action, commit)
+
+        with Phase() as phase:
+            # make sure properties to be set exist in the object's class
+            self._validate_object_properties(
+                phase, action, schema, obj.klass.name, action.properties)
+
+            # make sure none of the properties to set are raw properties
+            # as we have separate actions for them
+            self._validate_object_properties_not_raw(
+                phase, action, schema, obj.klass.name, action.properties)
+
+        # build a new object tree
+        object_tree = self._update_object_tree(obj, action.properties)
+
+        # build a new class tree with the object changed
+        class_entry = tree[obj.klass.name]
+        class_tree = self.store.repo[class_entry.oid]
+        builder = self.store.repo.TreeBuilder(class_tree)
+        builder.insert(obj.uuid, object_tree.oid, pygit2.GIT_FILEMODE_TREE)
+        new_class_oid = builder.write()
+
+        # build and return a new overall store tree
+        builder = self.store.repo.TreeBuilder(tree)
+        builder.insert(obj.klass.name, new_class_oid, pygit2.GIT_FILEMODE_TREE)
+        new_tree_oid = builder.write()
+        new_tree = self.store.repo[new_tree_oid]
+        klass = self.store._klass(new_tree, obj.klass.name)
+        updated_obj = self.store._object(new_tree, obj.uuid, klass)
+        return updated_obj, new_tree
 
     def _validate_object_class(self, action, klass, schema):
         if not klass in schema.classes:
             raise validation.ActionClassUnknownError(action, schema, klass)
 
-    def _validate_object_properties(self, action, schema, phase):
-        for prop_name in action.properties.iterkeys():
-            if not prop_name in schema.classes[action.klass].properties:
+    def _validate_object_properties(
+            self, phase, action, schema, klass, properties):
+        for prop_name in properties.iterkeys():
+            if not prop_name in schema.classes[klass].properties:
                 phase.error(validation.ActionPropertyUnknownError(
-                    action, schema, action.klass, prop_name))
+                    action, schema, klass, prop_name))
 
-    def _validate_object_properties_not_raw(self, action, schema, phase):
-        for prop_name in action.properties.iterkeys():
-            if prop_name in schema.classes[action.klass].properties:
-                prop = schema.classes[action.klass].properties[prop_name]
+    def _validate_object_properties_not_raw(
+            self, phase, action, schema, klass, properties):
+        for prop_name in properties.iterkeys():
+            if prop_name in schema.classes[klass].properties:
+                prop = schema.classes[klass].properties[prop_name]
                 if isinstance(prop, definitions.RawPropertyDefinition):
                     phase.error(validation.ActionIllegalRawPropertyChangeError(
-                        action, schema, action.klass, prop_name))
+                        action, schema, klass, prop_name))
+
+    def _validate_and_resolve_target_object(self, action, commit):
+        if action.action_id is not None:
+            target_action = self._validate_and_resolve_target_action(action)
+            obj = self.action_objects[target_action]
+            print repr(obj), obj
+            return obj
+        else:
+            objects = self.store.objects(commit)
+            for class_objects in objects.itervalues():
+                for obj in class_objects:
+                    if obj.uuid == action.uuid:
+                        return obj
+            raise validation.ActionReferencesNonExistentObject(action, schema)
+
+    def _validate_and_resolve_target_action(self, action):
+        actions = \
+            [a for a in self.transaction.actions if a.id == action.action_id]
+        if not actions:
+            raise validation.ActionReferenceNonExistentActionError(
+                action, schema)
+        target_action = actions[0]
+        if not target_action in self.action_objects:
+            raise validation.ActionReferencesLaterActionError(action, schema)
+        return target_action
 
     def _create_object_tree(self, uuid, properties):
         # generate YAML data to write into <class>/<uuid>/properties.yaml
@@ -418,6 +505,31 @@ class TransactionPreparer(object):
         yaml_data = yaml.dump(
             data, Dumper=yaml.CDumper, default_flow_style=False)
 
+        # generate the properties.yaml blob
+        blob_oid = self.store.repo.create_blob(yaml_data)
+
+        # generate and return the object tree
+        builder = self.store.repo.TreeBuilder()
+        builder.insert('properties.yaml', blob_oid, pygit2.GIT_FILEMODE_BLOB)
+        tree_oid = builder.write()
+        return self.store.repo[tree_oid]
+
+    def _update_object_tree(self, obj, properties):
+        # generate YAML data to write into <class>/<uuid>/properties.yaml
+        data = {}
+        for prop_name, prop in obj.properties.iteritems():
+            data[prop_name] = prop.value
+        for prop_name, prop in properties.iteritems():
+            # delete properties that are set to None in the action,
+            # overwrite the others
+            if prop_name in data and prop.value is None:
+                del data[prop_name]
+            else:
+                data[prop_name] = prop.value
+        yaml_data = yaml.dump(
+            data, Dumper=yaml.CDumper, default_flow_style=False)
+
+        # generate the properties.yaml blob
         # generate the properties.yaml blob
         blob_oid = self.store.repo.create_blob(yaml_data)
 
