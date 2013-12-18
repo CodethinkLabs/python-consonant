b1fd3330109dd4e74a9e5a4f504f2ea7f2483941
Jannis Pohlmann <jannis.pohlmann@codethink.co.uk>
2013-12-18 14:09:01 +0000

    Remove && from the shell script

--- .travis.sh
+++ .travis.sh
@@ -5,13 +5,13 @@ set -e
 sudo apt-get install cmake
 
 cd ~
-&& git clone --depth=1 -b master https://github.com/libgit2/libgit2.git
-&& ls .
-&& cd libgit2
-&& mkdir build
-&& cd build
-&& cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DBUILD_CLAR=OFF
-&& cmake --build . --target install
-&& ls ..
+git clone --depth=1 -b master https://github.com/libgit2/libgit2.git
+ls .
+cd libgit2
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=../_install -DBUILD_CLAR=OFF
+cmake --build . --target install
+ls ..
 
 pip install pygit2
