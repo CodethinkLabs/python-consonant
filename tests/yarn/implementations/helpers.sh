bc3b548359ac32cf50bd3a885f810c49dc9ebf2e
Jannis Pohlmann <jannis.pohlmann@codethink.co.uk>
2014-01-30 17:44:52 +0000

    Make run_python_test able to parse multiple YAML documents
    The first YAML document is assigned to the same test variable as
    before (output_yaml), the second to output_raw_properties, which
    is expected to be a dictionary mapping object UUIDs to dictionaries
    mapping raw property names to raw property data:
    
        <uuid 1>:
          foo: <raw data>
          bar: <raw data>
        <uuid 2>:
          foo: <raw data>
    
    This can then be used to verify that specific raw properties hold
    expected raw data.

--- tests/yarn/implementations/helpers.sh
+++ tests/yarn/implementations/helpers.sh
@@ -118,7 +118,15 @@ import yaml
 
 output_raw = open('stdout').read()
 output_stripped = open('stdout').read().strip()
-output_yaml = yaml.load(open('stdout'))
+yaml_documents = list(yaml.load_all(open('stdout')))
+if not yaml_documents:
+    output_yaml = None
+else:
+    output_yaml = yaml_documents[0]
+if len(yaml_documents) < 2:
+    output_raw_properties = None
+else:
+    output_raw_properties = yaml_documents[1]
 
 print output_yaml
 
